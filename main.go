ec3f5c03fc4613fec108762b5e5f56e4d6d074e8
sungalex <cloudallera@gmail.com>
2021-01-01 16:40:44 +0900

    go methods

--- main.go
+++ main.go
@@ -8,5 +8,11 @@ import (
 
 func main() {
 	account := accounts.NewAccount("alex")
+	account.Deposit(10)
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	account.ChangeOwner("park")
 	fmt.Println(account)
 }
