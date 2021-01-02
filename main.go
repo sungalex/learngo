cc273f855f7a03ada82ca8ad5db44fb67aab7a9b
sungalex <cloudallera@gmail.com>
2021-01-02 11:01:02 +0900

    channels

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 )
 
 func main() {
-	go sexyCount("alex")
-	sexyCount("park")
+	c := make(chan bool)
+	people := [2]string{"alex", "park"}
+	for _, people := range people {
+		go isSexy(people, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
 
 // for URL Checker
