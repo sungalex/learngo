1d37d98c17f8ce5b422d517b749a995c798baa9d
sungalex <cloudallera@gmail.com>
2021-01-02 12:03:49 +0900

    URL Checker and Go Routins/Channels - Fast URLChecker

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-// Go routines and channels
 /*
+[Go routines and channels theory]
 	- 메인 함수가 종료하면 go routine이 끝나던 아니던 상관없이 프로그램이 종료된다.
 	- go routine은 Non-blocking Operation 이지만,
 	  channel을 수신하는 operation(<-)은 Blocking Operation 이다.
@@ -9,7 +9,59 @@ package main
 	- Channel를 생성할 때는 수신할 메시지의 Type을 지정해야 한다.
 	- Channel에서 message를 수신할 때는 한번에 하나의 메시지 만 수신된다.
 	  즉, go routine의 수 만큼 channel을 통해 message를 수신하는 "<-" Operation이 있어야 한다.
+	- 함수 선언 시 channel type에 "chan<-"을 사용하면 read-only channel이 된다.
 */
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type requestResult struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("Request Failed")
+
+func main() {
+	c := make(chan requestResult)
+	results := map[string]string{}
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) {
+	status := "OK"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
+}
+
+// go routine and channel - 1
+/*
 import (
 	"fmt"
 	"time"
@@ -31,6 +83,7 @@ func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 5)
 	c <- person + " is sexy"
 }
+*/
 
 // for URL Checker
 /*
